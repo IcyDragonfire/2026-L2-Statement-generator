dbe1219f62933ee8de9e119fad15bc856d894e4d
IcyDragonfireIcyDragonfire <turnerw3197@masseyhigh.school.nz>
2026-02-11 10:46:58 +1300

    Add files via upload

---
+++ C_01_Make_Statement_V2.py
@@ -0,0 +1,25 @@
+# Functions go here
+def make_statement(statement, decoration, lines):
+    """Emphasise headings by adding decoration"""
+
+    middle = f"{decoration * 5} {statement} {decoration * 5}"
+    top_bottom = decoration * len(middle)
+
+    if lines == 1:
+        print(middle)
+    elif lines == 2:
+        print(middle)
+        print(top_bottom)
+
+    else:
+        print(top_bottom)
+        print(middle)
+        print(top_bottom)
+
+
+# Main routine goes here
+make_statement("Cloud is in the sky", "=", 3)
+print()
+make_statement("Cloud is still in the sky", "*", 2)
+print()
+make_statement("Cloud in action", "☁️", 1)
